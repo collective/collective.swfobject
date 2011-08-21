34b6786c457a5352c91d91edc07d5dc29f17b6ec
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-08-21 23:31:00 +0200

    rename readme to rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='collective.swfobject',
       version=version,
       description="SWFObject for Plone",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
